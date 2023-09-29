f21037d5b44cf5b51da02955413f70b0a7957d7e
Ken Lee <ken.lee.dev@gmail.com>
2023-09-28 20:37:26 -0700

    Create snake object

--- snake.js
+++ snake.js
@@ -1,4 +1,28 @@
-function draw() {
+// snake obj. has props x, y, and dir
+// snake.slither();
+// snake.changeDir(dir);
+// snake.eat();
+
+class Node {
+  constructor(x, y, next = null) {
+    this.x = x;
+    this.y = y;
+    this.next = next;
+  }
+}
+
+class Snake {
+  constructor(x, y, dir = null) {
+    this.head = new Node(x, y);
+    this.tail = this.head;
+    this.dir = dir;
+  }
+  get position() {
+    return [this.head.x,this.head.y];
+  }
+}
+
+function drawBoard() {
   const ctx = document.getElementById("canvas").getContext("2d");
   for (let i = 2; i < 22; i++) {
     for (let j = 2; j < 22; j++) {
@@ -7,4 +31,15 @@ function draw() {
   }
 }
 
-draw();
+function drawSnake(snake) {
+  const ctx = document.getElementById("canvas").getContext("2d");
+  node = snake.head;
+  while (node) {
+    ctx.fillRect(node.x * 25, node.y * 25, 25, 25);
+    node = node.next;
+  }
+}
+
+drawBoard();
+const snake = new Snake(2, 2);
+drawSnake(snake);
